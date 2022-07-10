b343e4b33d87ceeedf86cae47540471f0af0db28
digitalhouse <digitalhouse@MacBook-Pro-de-digitalhouse.local>
2022-07-10 18:27:01 -0300

    add i18n client

--- app/client/productList.js
+++ app/client/productList.js
@@ -1,11 +1,5 @@
-/**
- * Styles
- */
 require('../pages/productList/styles.scss');
 
-/**
- * Module dependencies
- */
 const React = require('react');
 const hydrate = require('nordic/hydrate');
 const I18n = require('nordic/i18n');
@@ -13,18 +7,19 @@ const I18nProvider = require('nordic/i18n/I18nProvider');
 const ImageProvider = require('nordic/image/provider');
 const ProductListView = require('../pages/productList/view');
 
-
 const {
-    prodlist,
+    translations,
+    products,
     imagesPrefix
 } = window.__PRELOADED_STATE__;
 
-
-
+const i18n = new I18n({ translations })
 
 hydrate(
-    <ImageProvider prefix={imagesPrefix}>
-        <ProductListView prodlist={prodlist} />
-    </ImageProvider>,
+    <I18nProvider i18n={i18n}>
+        <ImageProvider prefix={imagesPrefix}>
+            <ProductListView products={products} />
+        </ImageProvider>
+    </I18nProvider>,
     document.getElementById('root-app'),
 );
