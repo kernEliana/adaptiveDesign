b1119cc95b88ef98a52a18594cfe5635ff5107ae
andyfrankmeli <ext_anfrank@mercadolibre.com>
2022-08-06 16:44:02 -0300

    adjust exc-3 productcard comments

--- app/components/ProductCard/index.js
+++ app/components/ProductCard/index.js
@@ -9,7 +9,7 @@
  * a renderizar.
  * 
  *******************************************************************
- * NOTA: una vez finalizado el ejercicio , correr el comando
+ * NOTA: una vez finalizado el ejercicio, correr el comando
  * `npm run test:unit:watch productcard` y corroborar que pasen 
  * todos los tests.
  *******************************************************************
@@ -22,14 +22,20 @@
 
 
 /**
-  * Clase 3 - Ejercitacion
-  * 
-  * Agregar un botón en cada card de producto con la leyenda “Agregar a favoritos”. 
-  * Al clickearlo agregará el producto a un listado de favoritos. 
-  * 
-  * Una vez que el producto esté agregado a favoritos, el texto del botón debe cambiar
-  * a “Quitar de favoritos” y debe permitir eliminar dicho producto de favoritos. Al 
-  * hacerlo el texto debe volver a cambiar y el botón nos debe permitir volver a agregar 
-  * el producto. Chequear que el listado de favoritos se modifica correctamente en cada 
-  * acción del usuario mediante la consola.
-  */
+ * Clase 3 - Ejercitación
+ * 
+ * Agregar un botón en cada card de producto con la leyenda “Agregar a favoritos”. 
+ * Al clickearlo agregará el producto a un listado de favoritos. 
+ * 
+ * Una vez que el producto esté agregado a favoritos, el texto del botón debe cambiar
+ * a “Quitar de favoritos” y debe permitir eliminar dicho producto de favoritos. Al 
+ * hacerlo, el texto debe volver a cambiar y el botón nos debe permitir volver a agregar 
+ * el producto. Chequear que el listado de favoritos se modifica correctamente en cada 
+ * acción del usuario mediante la consola.
+ * 
+ *********************************************************************
+ * NOTA: las consignas de esta ejercitación se testean todas corriendo
+ * los tests del componente ProductList. Dirigite al index.js de dicho
+ * componente donde encontrarás las instrucciones necesarias.
+ *********************************************************************
+ */
